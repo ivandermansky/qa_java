80f14fa61ae01249af9b7fb006dc407ceb8c2069
ivandermansky <ivan.dermansky@gmail.com>
2026-04-24 10:47:03 +0300

    24-04-26 Cat.java

--- src/main/java/com/example/Cat.java
+++ src/main/java/com/example/Cat.java
@@ -4,10 +4,18 @@ import java.util.List;
 
 public class Cat {
 
-    Predator predator;
+    com.example.Predator predator;
+    private int defaultKittensCount; // количество котят по умолчанию
 
-    public Cat(Feline feline) {
+    // Конструктор с инъекцией зависимости и параметром для количества котят
+    public Cat(Feline feline, int defaultKittensCount) {
         this.predator = feline;
+        this.defaultKittensCount = defaultKittensCount;
+    }
+
+    // Конструктор без параметра количества котят, устанавливает значение по умолчанию — 4
+    public Cat(Feline feline) {
+        this(feline, 4);
     }
 
     public String getSound() {
@@ -18,4 +26,13 @@ public class Cat {
         return predator.eatMeat();
     }
 
+    // Получает количество котят с использованием значения по умолчанию
+    public int getKittens() {
+        return getKittens(defaultKittensCount);
+    }
+
+    // Получает количество котят с указанным параметром
+    public int getKittens(int kittensCount) {
+        return kittensCount;
+    }
 }
