14615e53fe6f60a45794bc91c9e18a0f37985c74
ivandermansky <ivan.dermansky@gmail.com>
2026-04-24 10:47:43 +0300

    24-04-26 Feline.java

--- src/main/java/com/example/Feline.java
+++ src/main/java/com/example/Feline.java
@@ -4,22 +4,30 @@ import java.util.List;
 
 public class Feline extends Animal implements Predator {
 
+    private int defaultKittensCount; // количество котят по умолчанию
+
+    // Конструктор с параметром для установки количества котят
+    public Feline(int defaultKittensCount) {
+        this.defaultKittensCount = defaultKittensCount;
+    }
+
+    // Конструктор без параметров, устанавливает значение по умолчанию — 3
+    public Feline() {
+        this(3);
+    }
+
     @Override
     public List<String> eatMeat() throws Exception {
         return getFood("Хищник");
     }
 
-    @Override
-    public String getFamily() {
-        return "Кошачьи";
-    }
+
 
     public int getKittens() {
-        return getKittens(1);
+        return getKittens(defaultKittensCount);
     }
 
     public int getKittens(int kittensCount) {
         return kittensCount;
     }
-
 }
